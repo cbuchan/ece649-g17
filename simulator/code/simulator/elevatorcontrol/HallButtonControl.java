aa1f0937f26682edea401feb091e6a50a817022a
Collin Buchan <cbuchan@andrew.cmu.edu>
2012-10-04 19:59:27 -0400

    fixed HallButtonControl, now checks both doors at a given hallway

--- simulator/code/simulator/elevatorcontrol/HallButtonControl.java
+++ simulator/code/simulator/elevatorcontrol/HallButtonControl.java
@@ -1,6 +1,7 @@
 package simulator.elevatorcontrol;
 
 import jSimPack.SimTime;
+import simulator.elevatorcontrol.Utility.DoorClosedArray;
 import simulator.elevatormodules.AtFloorCanPayloadTranslator;
 import simulator.elevatormodules.DoorClosedCanPayloadTranslator;
 import simulator.framework.*;
@@ -32,34 +33,23 @@ public class HallButtonControl extends Controller {
     private ReadableHallCallPayload localHallCall;
     private WriteableHallLightPayload localHallLight;
 
-    //network interface
-    // receive hall call from the other button
-    private WriteableCanMailbox networkHallLightOut;
-    // translator for the hall light message -- this is a generic translator
-    private BooleanCanPayloadTranslator mHallLight;
-
-    // send network hall call
-    private WriteableCanMailbox networkHallCallOut;
-    // translator for the hall call message -- this is a generic translator
-    private BooleanCanPayloadTranslator mHallCall;
-
-    //received door closed message
+    //input network messages
     private ReadableCanMailbox networkDoorClosedFrontLeft;
-    //translator for the doorClosed message -- this translator is specific
-    //to this messages, and is provided the elevatormodules package
-    private DoorClosedCanPayloadTranslator mDoorClosedFrontLeft;
-
-    //received desired floor message
     private ReadableCanMailbox networkDesiredFloor;
-    //translator for the doorClosed message -- this translator is specific
-    //to this messages, and is provided the elevatormodules package
-    private DesiredFloorCanPayloadTranslator mDesiredFloor;
-
-    //received desired floor message
     private ReadableCanMailbox networkAtFloor;
-    //translator for the doorClosed message -- this translator is specific
-    //to this messages, and is provided the elevatormodules package
+    private DoorClosedArray networkDoorClosed;
+
+    //translators for input network messages
+    private BooleanCanPayloadTranslator mHallCall;
+    private DesiredFloorCanPayloadTranslator mDesiredFloor;
     private AtFloorCanPayloadTranslator mAtFloor;
+
+    //output network messages
+    private WriteableCanMailbox networkHallLightOut;
+    private WriteableCanMailbox networkHallCallOut;
+
+    //translators for output network messages
+    private BooleanCanPayloadTranslator mHallLight;
 
     //these variables keep track of which instance this is.
     private final Hallway hallway;
@@ -68,8 +58,6 @@ public class HallButtonControl extends Controller {
 
     //store the period for the controller
     private SimTime period;
-
-    //internal constant declarations
 
     //enumerate states
     private enum State {
@@ -108,75 +96,50 @@ public class HallButtonControl extends Controller {
         */
         log("Created HallButtonControl[", this.floor, "][", this.hallway, "][", this.direction, "]");
 
-        //initialize physical state
-        //create a payload object for this floor,hallway,direction using the
-        //static factory method in HallCallPayload.
+        //initialize physical state input
         localHallCall = HallCallPayload.getReadablePayload(floor, hallway, direction);
-        //register the payload with the physical interface (as in input) -- it will be updated
-        //periodically when the hall call button state is modified.
         physicalInterface.registerTimeTriggered(localHallCall);
 
-        //create a payload object for this floor,hallway,direction
-        //this is an output, so it is created with the Writeable static factory method
+        //initialize physical state output
         localHallLight = HallLightPayload.getWriteablePayload(floor, hallway, direction);
-        //register the payload to be sent periodically -- whatever value is stored
-        //in the localHallLight object will be sent out periodically with the period
-        //specified by the period parameter.
         physicalInterface.sendTimeTriggered(localHallLight, period);
 
-        //initialize network interface
-        //create a can mailbox - this object has the binary representation of the message data
-        //the CAN message ids are declared in the MessageDictionary class.  The ReplicationComputer
-        //class provides utility methods for computing offsets for replicated controllers
+        //create CAN mailbox for output network messages
         networkHallLightOut = CanMailbox.getWriteableCanMailbox(MessageDictionary.HALL_LIGHT_BASE_CAN_ID +
                 ReplicationComputer.computeReplicationId(floor, hallway, direction));
+        networkHallCallOut = CanMailbox.getWriteableCanMailbox(
+                MessageDictionary.HALL_CALL_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway, direction));
+
         /*
-         * Create a translator with a reference to the CanMailbox.  Use the
-         * translator to read and write values to the mailbox
-         *
-         * Note the use of the BooleanCanPayloadTranslator.  This translator, along with
-         * IntegerCanPayloadTranslator, are provided for your use.  They are not
-         * very bandwidth efficient, but they will be adequate for the first part
-         * of the course.  When we get to network scheduling, you may wish to write
-         * your own translators, although you can do so at any time.
-         */
+        * Create a translator with a reference to the CanMailbox.  Use the
+        * translator to read and write values to the mailbox
+        */
         mHallLight = new BooleanCanPayloadTranslator(networkHallLightOut);
+        mHallCall = new BooleanCanPayloadTranslator(networkHallCallOut);
+
         //register the mailbox to have its value broadcast on the network periodically
         //with a period specified by the period parameter.
         canInterface.sendTimeTriggered(networkHallLightOut, period);
-
-
-        // Registration of mHallCall message
-        networkHallCallOut = CanMailbox.getWriteableCanMailbox(
-                MessageDictionary.HALL_CALL_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway, direction));
-        mHallCall = new BooleanCanPayloadTranslator(networkHallCallOut);
         canInterface.sendTimeTriggered(networkHallCallOut, period);
 
         /*
-         * Registration for the mDoorClosed message is similar to the mHallLight message
-         *
          * To register for network messages from the smart sensors or other objects
          * defined in elevator modules, use the translators already defined in
          * elevatormodules package.  These translators are specific to one type
          * of message.
          */
-        networkDoorClosedFrontLeft = CanMailbox.getReadableCanMailbox(MessageDictionary.DOOR_CLOSED_SENSOR_BASE_CAN_ID +
-                ReplicationComputer.computeReplicationId(Hallway.FRONT, Side.LEFT));
-        mDoorClosedFrontLeft = new DoorClosedCanPayloadTranslator(networkDoorClosedFrontLeft, Hallway.FRONT, Side.LEFT);
+        networkDoorClosed = new Utility.DoorClosedArray(hallway, canInterface);
+        networkDesiredFloor = CanMailbox.getReadableCanMailbox(MessageDictionary.DESIRED_FLOOR_CAN_ID);
+        networkAtFloor = CanMailbox.getReadableCanMailbox(
+                MessageDictionary.AT_FLOOR_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway));
+
+        mAtFloor = new AtFloorCanPayloadTranslator(networkAtFloor, floor, hallway);
+        mDesiredFloor = new DesiredFloorCanPayloadTranslator(networkDesiredFloor);
+
         //register to receive periodic updates to the mailbox via the CAN network
         //the period of updates will be determined by the sender of the message
         canInterface.registerTimeTriggered(networkDoorClosedFrontLeft);
-
-
-        // Registration for the mDesiredFloor message
-        networkDesiredFloor = CanMailbox.getReadableCanMailbox(MessageDictionary.DESIRED_FLOOR_CAN_ID);
-        mDesiredFloor = new DesiredFloorCanPayloadTranslator(networkDesiredFloor);
         canInterface.registerTimeTriggered(networkDesiredFloor);
-
-        // Registration for the mAtFloor message
-        networkAtFloor = CanMailbox.getReadableCanMailbox(
-                MessageDictionary.AT_FLOOR_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway));
-        mAtFloor = new AtFloorCanPayloadTranslator(networkAtFloor, floor, hallway);
         canInterface.registerTimeTriggered(networkAtFloor);
 
         /* issuing the timer start method with no callback data means a NULL value
@@ -219,7 +182,7 @@ public class HallButtonControl extends Controller {
 
                 //transitions -- note that transition conditions are mutually exclusive
                 //#transition 'T8.2'
-                if (mDoorClosedFrontLeft.getValue() == false && mAtFloor.getValue() == true && mDesiredFloor.getFloor() == floor &&
+                if (networkDoorClosed.getBothClosed() == false && mAtFloor.getValue() == true && mDesiredFloor.getFloor() == floor &&
                         (mDesiredFloor.getDirection() == Direction.STOP || mDesiredFloor.getDirection() == direction)) {
                     newState = State.STATE_HALL_CALL_OFF;
                 } else {
