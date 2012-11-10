f90b8605ec0f697aee3587027cfd96b96d30d8b5
Rajeev Sharma <rdsharma3@gmail.com>
2012-11-10 17:52:39 -0500

    New CAN IDs

--- simulator/code/simulator/elevatorcontrol/MessageDictionary.java
+++ simulator/code/simulator/elevatorcontrol/MessageDictionary.java
@@ -35,28 +35,28 @@ public class MessageDictionary {
     public final static SimTime DRIVE_CONTROL_PERIOD = new SimTime(10, SimTimeUnit.MILLISECOND);
 
     //controller message IDs
-    public final static int DRIVE_SPEED_CAN_ID =                0x0F09B500;
-    public final static int DRIVE_COMMAND_CAN_ID =              0x0F0AB500;
-    public final static int DESIRED_DWELL_BASE_CAN_ID =         0x0F0BB600;
-    public final static int DESIRED_FLOOR_CAN_ID =              0x0F0CB600;
-    public final static int CAR_POSITION_CAN_ID =               0x0F0DB700;
-    public final static int DOOR_MOTOR_COMMAND_BASE_CAN_ID =    0x0F0EB800;
-    public final static int HALL_CALL_BASE_CAN_ID =             0x0F0FB900;
+    public final static int DRIVE_SPEED_CAN_ID =                0x0C4CB500;
+    public final static int DRIVE_COMMAND_CAN_ID =              0x0CB0B500;
+    public final static int DESIRED_DWELL_BASE_CAN_ID =         0x0F08B600;
+    public final static int DESIRED_FLOOR_CAN_ID =              0x0F09B600;
+    public final static int CAR_POSITION_CAN_ID =               0x0E403C00;
+    public final static int DOOR_MOTOR_COMMAND_BASE_CAN_ID =    0x0D14B800;
+    public final static int HALL_CALL_BASE_CAN_ID =             0x0F0BB900;
     public final static int HALL_LIGHT_BASE_CAN_ID =            0x0F10B900;
-    public final static int CAR_CALL_BASE_CAN_ID =              0x0F11BA00;
+    public final static int CAR_CALL_BASE_CAN_ID =              0x0F0CBA00;
     public final static int CAR_LIGHT_BASE_CAN_ID =             0x0F12BA00;
-    public final static int CAR_LANTERN_BASE_CAN_ID =           0x0F13BB00;
+    public final static int CAR_LANTERN_BASE_CAN_ID =           0x0F0BBB00;
     
     //module message IDs
-    public final static int AT_FLOOR_BASE_CAN_ID =              0x0C4C2800;
-    public final static int CAR_LEVEL_POSITION_CAN_ID =         0x0CB03C00;
-    public final static int CAR_WEIGHT_CAN_ID =                 0x0DDC7800;
-    public final static int CAR_WEIGHT_ALARM_CAN_ID =           0x0E408C00;
-    public final static int DOOR_OPEN_SENSOR_BASE_CAN_ID =      0x0EA4A000;
-    public final static int DOOR_CLOSED_SENSOR_BASE_CAN_ID =    0x0D145000;
-    public final static int DOOR_REVERSAL_SENSOR_BASE_CAN_ID =  0x0D786400;
-    public final static int HOISTWAY_LIMIT_BASE_CAN_ID =        0x0F08B400;
-    public final static int EMERGENCY_BRAKE_CAN_ID =            0x0BE81400;
-    public final static int LEVELING_BASE_CAN_ID =                   0x0AE81400;
+    public final static int AT_FLOOR_BASE_CAN_ID =              0x0DDC2800;
+    public final static int CAR_LEVEL_POSITION_CAN_ID =         0x0EA43C00;
+    public final static int CAR_WEIGHT_CAN_ID =                 0x0F0D7800;
+    public final static int CAR_WEIGHT_ALARM_CAN_ID =           0x0F0E8C00;
+    public final static int DOOR_OPEN_SENSOR_BASE_CAN_ID =      0x0F09A000;
+    public final static int DOOR_CLOSED_SENSOR_BASE_CAN_ID =    0x0F0A5000;
+    public final static int DOOR_REVERSAL_SENSOR_BASE_CAN_ID =  0x0BE86400;
+    public final static int HOISTWAY_LIMIT_BASE_CAN_ID =        0x0CB0B400;
+    public final static int EMERGENCY_BRAKE_CAN_ID =            0x0D781400;
+    public final static int LEVELING_BASE_CAN_ID =                   0x0F08B400;
     
 }
